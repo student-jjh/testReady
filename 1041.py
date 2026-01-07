50de7719cc7afe09562057c7d36d2ed4124fcb45
JJH <skkuwjswnsgur456@gmail.com>
2026-01-07 23:09:30 +0900

    today's gold

---
+++ 1041.py
@@ -0,0 +1,28 @@
+N = int(input())
+    arr = list(map(int, sys.stdin.readline().split()))
+    ans = 0
+    min_lists = []
+    if N == 1:
+        arr.sort()
+        for i in range(5):
+            ans += arr[i]
+    else:
+        min_lists.append(min(arr[0], arr[5]))
+        min_lists.append(min(arr[1], arr[4]))
+        min_lists.append(min(arr[2], arr[3]))
+        min_lists.sort()
+
+        # 1, 2, 3 면의 주사위 최소값
+        min1 = min_lists[0]
+        min2 = min_lists[0] + min_lists[1]
+        min3 = sum(min_lists)
+
+        # 1, 2, 3 면의 주사위 개수
+        n1 = 4 * (N - 2) * (N - 1) + (N - 2) ** 2
+        n2 = 4 * (N - 1) + 4 * (N - 2)
+        n3 = 4
+
+        ans += min1 * n1
+        ans += min2 * n2
+        ans += min3 * n3
+    print(ans)
